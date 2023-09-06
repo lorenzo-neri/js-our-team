19167ae96d4026a33e5e30fdd567831d2fc68620
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-06 17:26:38 +0200

    add in console result of form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 /* 
-
 Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 MILESTONE 0:
 Creare l’array di oggetti con le informazioni fornite.
@@ -11,7 +10,6 @@ BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede
-
 
 Dati:
 - Wayne Barnett
@@ -32,56 +30,48 @@ scott-estrada-developer.jpg
 -Barbara Ramos	
 Graphic Designer	
 barbara-ramos-graphic-designer.jpg
-
 */
 
 //creo l'array di oggetti per rappresentare i membri del team
 //array = team
 //objects = members
 let team = [
-
     //[0]
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
         image: './assets/img/wayne-barnett-founder-ceo.jpg'
     },
-
     //[1]
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
         image: './assets/img/angela-caroll-chief-editor.jpg'
     },
-
     //[2]
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
         image: './assets/img/walter-gordon-office-manager.jpg'
     },
-
     //[3]
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
         image: './assets/img/angela-lopez-social-media-manager.jpg'
     },
-
     //[4]
     {
         name: 'Scott Estrada',
         role: 'Developer',
         image: './assets/img/scott-estrada-developer.jpg'
     },
-
     //[5]
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
         image: './assets/img/barbara-ramos-graphic-designer.jpg'
     }
-
 ]
 
 //stampo array
@@ -115,7 +105,7 @@ for (let i = 0; i < team.length; i++) {
     const member = team[i];
     console.log(member);
     const markupEl =
-        `<div class="col-4">
+        `<div class="col-12 col-md-4">
             <div class="card p-2 m-3 bg-info">
                 <h5>${member['name']}</h5>
                 <div>${member['role']}</div>
@@ -125,4 +115,22 @@ for (let i = 0; i < team.length; i++) {
     teamEl.insertAdjacentHTML('beforeend', markupEl);
 
 }
+
+/*
+Bonus aggiuntivo:
+Fare un form che permetta di aggiungere un nuovo membro del team.
+Per l’immagine va bene usare semplicemente una url causale da picsum oppure anche la stesse immagine. 
+*/
+
+let formEl = addEventListener('submit', function (e){
+    
+    e.preventDefault(); //solo per fare prove, poi togliere
+    
+    //console.log('enter on form');
+    
+    const fullNameEl = document.getElementById('full_name').value;
+    const roleEl = this.document.getElementById('role').value;
+    console.log(`Full Name: ${fullNameEl} Role: ${roleEl}`);
+    
+})
 
